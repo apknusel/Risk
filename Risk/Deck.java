ac6bd31bc2a4cbe0277623a0ca6f296505305fb5
Alex Knusel <apknusel@stu.naperville203.org>
2019-05-06 14:47:10 -0500

    Made Deck

---
+++ Risk/Deck.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+
+public class Deck
+{
+	private int i;
+	private String input;
+	private String name;
+	private String[] typesArray;
+	private ArrayList<Card> deck;
+	private ArrayList<Country> countries;
+	private Card drawCard;
+
+	/**
+	* Creates all 42 cards, one for each territory. Each card has either 
+	* a type of Infantry, Cavalry, or Artillery. Ensure that the number of
+	* Infantry, Cavalry, and Artillery are the same
+	**/
+	public Deck (ArrayList<Country> countries)
+	{		
+		Collections.shuffle(countries);
+		typesArray = new String[]{ "Infantry", "Cavalry", "Artillery" };
+		deck = new ArrayList<Card>();
+		for (i = 0; i < countries.size(); i++)
+		{
+			deck.add(new Card(typesArray[i / 14], countries.get(i)));
+			System.out.println("Added new card to deck: " + deck.get(i).getName());
+		}
+		Collections.shuffle(deck);
+	}
+	/**
+	* Removes a card from the deck and return it
+	**/
+	public Card draw()
+	{
+		drawCard = deck.get(0);
+		deck.remove(0);
+		return drawCard;
+	}
+	/**
+	* Add a card to the deck
+	**/
+	public void add(Card card)
+	{
+		deck.add(card);
+	}
+	/**
+	* Shuffle the deck of cards
+	**/
+	public void shuffle()
+	{
+		Collections.shuffle(deck);
+	}
+}
--- Risk/RiskMainPanel.java
+++ Risk/RiskMainPanel.java
@@ -7,19 +7,23 @@ public class RiskMainPanel {
         JPanel panel=new JPanel();  
         panel.setBounds(40,80,200,200);    
         panel.setBackground(Color.blue);  
-        JButton b1=new JButton("Button 1");     
+        JButton b1=new JButton("New Game");     
         b1.setBounds(50,100,80,30);    
         b1.setBackground(Color.yellow);   
-        JButton b2=new JButton("Button 2");   
+        JButton b2=new JButton("Load Game");   
         b2.setBounds(100,100,80,30);    
-        b2.setBackground(Color.green);   
+        b2.setBackground(Color.green);
+        JButton b3=new JButton("Quit");   
+        b3.setBounds(100,100,80,30);    
+        b3.setBackground(Color.green);
         panel.add(b1);
-        panel.add(b2);  
+        panel.add(b2);
+        panel.add(b3);
         f.add(panel);  
         f.setSize(400,400);    
         f.setLayout(null);    
         f.setVisible(true);    
-        //s
+        
      }  
      public static void main(String args[])  
      {  
