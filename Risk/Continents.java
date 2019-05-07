cee7bd68caafd51c34a29089ea4c5de864b7271b
awgraham <42479487+awgraham@users.noreply.github.com>
2019-05-07 15:07:19 -0500

    Worked on classes

---
+++ Risk/Continents.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Continents
+{
+    ArrayList<Continent> Continents = new ArrayList<>();
+    public void CountryAdd()
+    {
+        Countries.add(Alaska);
+        Countries.add(Alberta);
+        Countries.add(CentralAmerica);
+        Countries.add(EasternUnitedStates);
+        Countries.add(Greenland);
+        Countries.add(NorthwestTerritory);
+        Countries.add(Ontario);
+        Countries.add(Quebec);
+        Countries.add(WesternUnitedStates);
+        Countries.add(Argentina);
+        Countries.add(Brazil);
+        Countries.add(Peru);
+        Countries.add(Venezuela);
+        Countries.add(GreatBritain);
+        Countries.add(Iceland);
+        Countries.add(NorthernEurope);
+        Countries.add(Scandanavia);
+        Countries.add(SouthernEurope);
+        Countries.add(Ukraine);
+        Countries.add(WesternEurope);
+        Countries.add(Congo);
+        Countries.add(EastAfrica);
+        Countries.add(Egypt);
+        Countries.add(Madagascar);
+        Countries.add(NorthAfrica);
+        Countries.add(SouthAfrica);
+        Countries.add(Afghanistan);
+        Countries.add(China);
+        Countries.add(India);
+        Countries.add(Irkutsk);
+        Countries.add(Japan);
+        Countries.add(Kamchatka);
+        Countries.add(MiddleEast);
+        Countries.add(Mongolia);
+        Countries.add(Siam);
+        Countries.add(Siberia);
+        Countries.add(Ural);
+        Countries.add(Yakutsk);
+        Countries.add(EasternAustralia);
+        Countries.add(Indonesia);
+        Countries.add(NewGuinea);
+        Countries.add(WesternAustralia);
+    }
+}
