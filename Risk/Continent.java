a574410bcbf725da2129e4d6caf6001a902b548b
Alex Knusel <apknusel@stu.naperville203.org>
2019-05-08 15:10:02 -0500

    Finished Continent

--- Risk/Continent.java
+++ Risk/Continent.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
 public class Continent
 {
-    int troopsgained;
-    
-    public Continent(int troopsgained)
+    private String name;
+    private int bArmies;
+    private ArrayList<Country> countries;
+    public Continent(String name, int bArmies, ArrayList<Country> memberCountries)
     {
-        troopsgained = troopsgained;
+		this.name = name;
+		this.bArmies = bArmies;
+		countries = memberCountries;
+		System.out.println("Created continent: " + name + "\nBonus armies: " + bArmies);
+    }
+    public String getName()
+    {
+		return name;
+    }
+    public int getBonusArmies()
+    {
+		return bArmies;
+    }
+    public ArrayList<Country> getCountries()
+    {
+		return countries;
     }
 }
