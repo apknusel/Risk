b6c52a8be91a05f3b13b49a5c2dbd68a347c3eb2
Arthur Graham <42479487+awgraham@users.noreply.github.com>
2019-05-07 21:55:28 -0500

    Worked on countries

--- Risk/Country.java
+++ Risk/Country.java
@@ -2,18 +2,61 @@
 public class Country
 {
     String name;
+    String occupant;
     String continent;
     int troops;
+    Country[] adjacentCountries;
     
     public Country(String name, String continent, int troops)
     {
         name = name;
-        continent = continent;
         troops = troops;
+        continent = continent;
+        occupant = "None";
+    }
+    
+    public void setAdjacentCountries(Country[] adjacentCountries)
+    {
+        adjacentCountries = adjacentCountries;
+    }
+    
+    public void setOccupant(String player)
+    {
+        occupant = player;
+    }
+    
+    public String getOccupant()
+    {
+        return occupant;
+    }
+
+    public void addTroops(int soldiers)
+    {
+        troops += soldiers;
+    }
+    
+    public void removeTroops(int soldiers)
+    {
+        troops -= soldiers;
+    }
+    
+    public int getTroops()
+    {
+        return troops;
     }
     
     public String getName()
     {
         return name;
+    }
+    
+    public Country[] getAdjacentCountries()
+    {
+        return adjacentCountries;
+    }
+    
+    public String getContinent(String continent)
+    {
+        return continent;
     }
 }
