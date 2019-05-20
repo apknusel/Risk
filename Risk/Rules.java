8d35cfb2bd96596ac6dda31227a4010f1a85144b
awgraham <42479487+awgraham@users.noreply.github.com>
2019-05-20 15:07:38 -0500

    Worked on stuff

---
+++ Risk/Rules.java
@@ -0,0 +1,48 @@
+public class Rules
+{
+    boolean gameOver = false;
+    Player currentPlayer;
+    Player nonCurrentPlayer;
+    Player tempPlayer;
+    public Rules()
+    {
+    }
+    
+    
+    
+    public static void main(String[] args)
+    {
+        
+        
+        while (gameOver != false)
+        {
+           tempPlayer = currentTroop 
+        }
+    }
+
+    //     Army army1 = new Army("Arthur");
+    //     Army army2 = new Army("Alex");
+    //     Player player1 = new Player(army1);
+    //     Player player2 = new Player(army2);
+    //     CountriesContinents cc = new CountriesContinents();
+    //     cc.setAdjacentCountries();
+    //     Country Alaska = cc.getCountry("Alaska");
+    //     Country Congo = cc.getCountry("Congo");
+    //     Troop firstTroop = new Troop("Artillery", Alaska);
+    //     Troop secondTroop = new Troop("Artillery", Alaska);
+    //     Troop thirdTroop = new Troop("Artillery", Alaska);
+    //     Troop fourthTroop = new Troop("Artillery", Alaska);
+    //     arthursArmy.addTroopToArmy(firstTroop);
+    //     arthursArmy.addTroopToArmy(secondTroop);
+    //     arthursArmy.addTroopToArmy(thirdTroop);
+    //     arthursArmy.addTroopToArmy(fourthTroop);
+    //     Country Alberta = cc.getCountry("Alberta");
+    //     Troop fifthTroop = new Troop("Artillery", Alberta);
+    //     alexsArmy.addTroopToArmy(fifthTroop);
+    //     System.out.println(arthursArmy.sizeOfArmy());
+    //     System.out.println(alexsArmy.sizeOfArmy());
+    //     arthursArmy.battleOfTroops(arthur, alex, Alaska, Alberta);
+    //     System.out.println(arthursArmy.sizeOfArmy());
+    //     System.out.println(alexsArmy.sizeOfArmy());
+    
+}
