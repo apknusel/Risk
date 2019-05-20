c3d9e16295636a663a48b70ebcdb22c63c490c37
Alex Knusel <apknusel@stu.naperville203.org>
2019-05-20 15:09:46 -0500

    Worked on putting rules in

--- Risk/Player.java
+++ Risk/Player.java
@@ -1,12 +1,10 @@
 public class Player
 {
     Army army;
-    
-    public Player(Army playersArmy)
+    public Player(Army playersArmy, String n)
     {
         army = playersArmy;
     }
-
     public Army getArmy()
     {
         return army;
---
+++ Risk/Rules2.java
@@ -0,0 +1,10 @@
+public class Rules2
+{
+    public Rules2()
+    {
+    }
+    public abstract giveTroops(Army a, Country c, String country)
+    {
+        a.addTroops(Troop);
+    }
+}
