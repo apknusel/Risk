5f892f345507143b0021af2227c14eec0dae67a5
Alex Knusel <apknusel@stu.naperville203.org>
2019-05-21 14:58:48 -0500

    Worked on stuff

--- Risk/Player.java
+++ Risk/Player.java
@@ -1,12 +1,39 @@
 public class Player
 {
     Army army;
+    boolean winner;
+    boolean turn;
     public Player(Army playersArmy, String n)
     {
         army = playersArmy;
+        winner = false;
+        turn = false;
     }
     public Army getArmy()
     {
         return army;
+    }
+    public void updateWinner(Player p)
+    {
+        if (army.sizeOfArmy() == 0 && p.getArmy().sizeOfArmy() == 0)
+        {
+            winner = false;;
+        }
+        else if (p.getArmy().sizeOfArmy() == 0)
+        {
+            winner = true;
+        }
+    }
+    public boolean isWinner()
+    {
+        return winner;
+    }
+    public void switchTurn()
+    {
+        turn = !turn;
+    }
+    public boolean getTurn()
+    {
+        return turn;
     }
 }
